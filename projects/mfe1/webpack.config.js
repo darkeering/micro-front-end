52ffaa990d797d04b806502eadf904cf81f2ef2d
darkeering <darkeering@outlook.com>
2023-06-24 14:01:00 +0800

    add font plugin

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -2,6 +2,52 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const mf = require("@angular-architects/module-federation/webpack");
 const path = require("path");
 const share = mf.share;
+const webpack = require('webpack')
+
+
+const set = new Set();
+const regexpDev = /url\('([^;\)]*)\.(ttf|woff2?|eot|otf|svg)(\?(.*))?'\)/g;
+const regexpProd = /url\(([^;\)]*)\.(ttf|woff2?|eot|otf|svg)(\?(.*))?\)/g;
+class OutputFontPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap("outputFont", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: "outputFont",
+          stage: webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE,
+        },
+        (assets) => {
+          for (let key in assets) {
+            if (/\.(ttf|woff2?|eot|otf)$/.test(key) && !set.has(key)) {
+              let path = "assets/mfe1/" + key;
+              assets[path] = assets[key];
+              set.add(path);
+            }
+
+            if (/(app.component.css)$/.test(key)) {
+              let regexp =
+                compilation.options.mode === "production"
+                  ? regexpProd
+                  : regexpDev;
+
+              if (assets[key]._value) {
+                let _value = assets[key]._value;
+                regexp.test(_value);
+                let temp = _value.replace(regexp, `url('assets/mfe1/$1.$2')`);
+                assets[key]._value = temp;
+              } else {
+                let _value = assets[key]._valueAsBuffer.toString();
+                regexp.test(_value);
+                let temp = _value.replace(regexp, `url('assets/mfe1/$1.$2')`);
+                assets[key]._valueAsBuffer = Buffer.from(temp);
+              }
+            }
+          }
+        }
+      );
+    });
+  }
+}
 
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
@@ -25,6 +71,7 @@ module.exports = {
     outputModule: true
   },
   plugins: [
+    new OutputFontPlugin(),
     new ModuleFederationPlugin({
         library: { type: "module" },
 
